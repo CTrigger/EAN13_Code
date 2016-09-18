a5816589edb63e62e990a76e0132496d1123639f
Ricardo Kim <hexadecimal.exe@gmail.com>
2016-09-18 20:29:26 -0300

    All Bar Codes Redesigned
    100% Working

--- EAN13 Code/Class/EAN13.cs
+++ EAN13 Code/Class/EAN13.cs
@@ -12,10 +12,6 @@ namespace EAN13_Code.Class
         private int Impar;
         private int Par;
 
-        //private char[] R;
-        //private char[] G;
-        //private char[] L;
-
         public string FinalEAN;
         public string BarCodeEAN;
         public string group;
@@ -23,7 +19,6 @@ namespace EAN13_Code.Class
 
         public string Digit;
         public string BaseEAN;
-        public string First;
 
 
         public EAN13(string EAN)
@@ -37,13 +32,13 @@ namespace EAN13_Code.Class
             char[] r    = { 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y' };
 
             char[] G    = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            char[] g    = { 'J', 'K', 'L', 'M', 'N', 'O', 'j', 'k', 'l', 'm' };
 
             char[] L    = { 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' };
             char[] l    = { 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' };
             char[] first = { '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*' };
             char[] Code = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2' };
 
-            
             for (int i = 0; i < EAN.Length; i++)
             {
                 
@@ -60,11 +55,7 @@ namespace EAN13_Code.Class
             }
             Impar = Impar * 3;
 
-            Digit = Convert.ToString(10 - ((Par + Impar) % 10));
-            if (Digit == "10")
-            {
-                Digit = "2";
-            }
+            Digit = Convert.ToString((10 - ((Par + Impar) % 10)) % 10);
 
             FinalEAN = BaseEAN + Digit;
 
@@ -75,13 +66,21 @@ namespace EAN13_Code.Class
                     //Tratamento do Grupo L
                     if (i == 1)
                     {
-                        Code[i] = l[(int)Char.GetNumericValue(FinalEAN[i])];
-                        group = group + "L";
+                        if (FinalEAN[i] != '1')
+                        {
+                            Code[i] = l[(int)Char.GetNumericValue(FinalEAN[i])];
+                            group = group + "L";
+                        }
+                        else
+                        {
+                            Code[i] = g[(int)Char.GetNumericValue(FinalEAN[i])];
+                            group = group + "G";
+                        }
                     }
 
                     if (i == 2)
                     {
-                        if ((int)Char.GetNumericValue(FinalEAN[i]) < 4)
+                        if (FinalEAN[i] == '0' || FinalEAN[i] == '2'|| FinalEAN[i] == '3')
                         {
                             Code[i] = L[(int)Char.GetNumericValue(FinalEAN[i])];
                             group = group + "L";
@@ -95,7 +94,7 @@ namespace EAN13_Code.Class
 
                     if (i==3)
                     {
-                        if (FinalEAN[i] == '0' || FinalEAN[i] == '3' || FinalEAN[i] == '4' || FinalEAN[i] == '7' || FinalEAN[i] == '8')
+                        if (FinalEAN[i] == '0' || FinalEAN[i] == '1' || FinalEAN[i] == '4' || FinalEAN[i] == '7' || FinalEAN[i] == '8')
                         {
                             Code[i] = L[(int)Char.GetNumericValue(FinalEAN[i])];
                             group = group + "L";
@@ -109,7 +108,7 @@ namespace EAN13_Code.Class
 
                     if (i == 4)
                     {
-                        if (FinalEAN[i] == '0' || FinalEAN[i] == '1' || FinalEAN[i] == '3' || FinalEAN[i] == '4' || FinalEAN[i] == '5' || FinalEAN[i] == '9')
+                        if (FinalEAN[i] == '0' || FinalEAN[i] == '4' || FinalEAN[i] == '5' || FinalEAN[i] == '9')
                         {
                             Code[i] = L[(int)Char.GetNumericValue(FinalEAN[i])];
                             group = group + "L";
@@ -123,7 +122,7 @@ namespace EAN13_Code.Class
 
                     if (i == 5)
                     {
-                        if (FinalEAN[i] == '0' || FinalEAN[i] == '2'|| FinalEAN[i] == '5' || FinalEAN[i] == '6' || FinalEAN[i] == '7')
+                        if (FinalEAN[i] == '0' || FinalEAN[i] == '1' || FinalEAN[i] == '2' || FinalEAN[i] == '5' || FinalEAN[i] == '6' || FinalEAN[i] == '7')
                         {
                             Code[i] = L[(int)Char.GetNumericValue(FinalEAN[i])];
                             group = group + "L";
@@ -137,7 +136,7 @@ namespace EAN13_Code.Class
 
                     if (i == 6)
                     {
-                        if (FinalEAN[i] == '0' || FinalEAN[i] == '3' || FinalEAN[i] == '6' || FinalEAN[i] == '8' || FinalEAN[i] == '9')
+                        if (FinalEAN[i] == '0' || FinalEAN[i] == '1' || FinalEAN[i] == '3' || FinalEAN[i] == '6' || FinalEAN[i] == '8' || FinalEAN[i] == '9')
                         {
                             Code[i] = L[(int)Char.GetNumericValue(FinalEAN[i])];
                             group = group + "L";
--- EAN13 Code/Program.cs
+++ EAN13 Code/Program.cs
@@ -15,63 +15,24 @@ namespace EAN13_Code
 {
     class Program
     {
-        private Image DrawText(String text, Font font, Color textColor, Color backColor)
-        {
-            //first, create a dummy bitmap just to get a graphics object
-            Image img = new Bitmap(1, 1);
-            Graphics drawing = Graphics.FromImage(img);
-
-            //measure the string to see how big the image needs to be
-            SizeF textSize = drawing.MeasureString(text, font);
-
-            //free up the dummy image and old graphics object
-            img.Dispose();
-            drawing.Dispose();
-
-            //create a new image of the right size
-            img = new Bitmap((int)textSize.Width, (int)textSize.Height);
-
-            drawing = Graphics.FromImage(img);
-
-            //paint the background
-            drawing.Clear(backColor);
-
-            //create a brush for the text
-            Brush textBrush = new SolidBrush(textColor);
-
-            drawing.DrawString(text, font, textBrush, 0, 0);
-
-            drawing.Save();
-
-            textBrush.Dispose();
-            drawing.Dispose();
-
-            return img;
-
-        }
+        
         static void Main(string[] args)
         {
             string a;
             Console.WriteLine("Inserir código de 12 digitos");
             a = Console.ReadLine();
-            //a = "abcdefghi jklmno pqrstuvxwy z 0123456789 |[]><?";
-            //a = "871125300120";
-            //a = "002345678901";
-            //a   = "999999999999";
-            //EAN13 x = new EAN13("111111111111");
+
+
             EAN13 x = new EAN13(a);
 
             PointF firstLocation = new PointF(10f, 10f);
 
-            //Bitmap bitmap = new Bitmap(x.FinalEAN.Length * 50, 250);
             Bitmap bitmap = new Bitmap(a.Length * 120, 550);
             using (Graphics graphics = Graphics.FromImage(bitmap))
             {
-                //using (Font oFont = new Font(@"./Fonts/EAN-13.ttf", 10))
                 using (Font oFont = new Font("EAN-13", 250))
                 {
                     //graphics.FillRectangle(Brushes.White, 0, 0, bitmap.Width, bitmap.Height);
-                    //graphics.DrawString(x.FinalEAN, oFont, Brushes.Black, firstLocation);
                     graphics.DrawString(x.BarCodeEAN, oFont, Brushes.Black, firstLocation);
                 }
             }
@@ -84,12 +45,6 @@ namespace EAN13_Code
             Console.WriteLine("Código Barras   " + x.BarCodeEAN);
             Console.WriteLine("Dígito final " + x.Digit);
             Console.WriteLine("Grupo:   " + x.group);
-
-            //using (Graphics graphics = Graphics.FromImage(bitmap))
-            //{
-            //    graphics.FillRectangle(, 0, 0, bitmap.Width, bitmap.Height);
-
-            //}
 
             Console.WriteLine("Pressione Qualquer tecla para sair");
             a = Console.ReadLine();
